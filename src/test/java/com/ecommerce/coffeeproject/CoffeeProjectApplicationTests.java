847b52888277855a512e664e25aa0a87d63e9cc5
최길중 <choil8228@gmail.com>
2026-05-04 23:57:18 +0900

    first commit

---
+++ src/test/java/com/ecommerce/coffeeproject/CoffeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.coffeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
