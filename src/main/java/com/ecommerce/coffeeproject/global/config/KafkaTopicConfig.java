49e85b04f66b6f1191e036888939a3f20d220151
최길중 <choil8228@gmail.com>
2026-05-08 00:26:49 +0900

    fix : Kafka Config 추가

---
+++ src/main/java/com/ecommerce/coffeeproject/global/config/KafkaTopicConfig.java
@@ -0,0 +1,21 @@
+package com.ecommerce.coffeeproject.global.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic orderCreatedTopic(
+            @Value("${app.kafka.topics.order-created}") String topicName
+    ) {
+        return TopicBuilder.name(topicName)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
