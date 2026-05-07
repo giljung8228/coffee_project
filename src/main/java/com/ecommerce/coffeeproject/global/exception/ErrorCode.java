788158e7bc4e35417244daae2e918a978e08ede2
최길중 <choil8228@gmail.com>
2026-05-07 11:32:10 +0900

    feat : ErrorCode 작성(인터페이스)

--- src/main/java/com/ecommerce/coffeeproject/global/exception/ErrorCode.java
+++ src/main/java/com/ecommerce/coffeeproject/global/exception/ErrorCode.java
@@ -1,26 +1,12 @@
 package com.ecommerce.coffeeproject.global.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-public enum ErrorCode {
+public interface ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "사용자를 찾을 수 없습니다."),
-    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "MENU_NOT_FOUND", "메뉴를 찾을 수 없습니다."),
-    POINT_NOT_FOUND(HttpStatus.BAD_REQUEST, "POINT_NOT_FOUND", "포인트를 먼저 충전해주세요."),
+    HttpStatus getStatus();
 
-    MENU_NOT_ON_SALE(HttpStatus.BAD_REQUEST, "MENU_NOT_ON_SALE", "현재 주문할 수 없는 메뉴입니다."),
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_REQUEST", "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다.");
+    String getCode();
 
-    private final HttpStatus status;
-    private final String code;
-    private final String message;
-
-    ErrorCode(HttpStatus status, String code, String message) {
-        this.status = status;
-        this.code = code;
-        this.message = message;
-    }
+    String getMessage();
 }
