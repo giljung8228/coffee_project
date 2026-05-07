faa3c17673a5754747402a81293d750634df8c8b
최길중 <choil8228@gmail.com>
2026-05-08 00:51:38 +0900

    feat : 데이터 플랫폼 Mock Client 구현

--- src/main/java/com/ecommerce/coffeeproject/domain/order/client/MockDataPlatformClient.java
+++ src/main/java/com/ecommerce/coffeeproject/domain/order/client/MockDataPlatformClient.java
@@ -1,9 +1,11 @@
 package com.ecommerce.coffeeproject.domain.order.client;
 
 import com.ecommerce.coffeeproject.domain.order.dto.OrderDataPlatformPayload;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "app.data-platform.client", havingValue = "mock")
 public class MockDataPlatformClient implements DataPlatformClient {
 
     @Override
